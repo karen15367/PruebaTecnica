2265a773aa7108d64aa751ea63e02ce316099e4c
Karen Sosa <l22020987@veracruz.tecnm.mx>
2026-02-12 14:08:55 -0600

    implementación de lógica en CharacterController.ts

--- rick-morty-api/src/controllers/CharacterController.ts
+++ rick-morty-api/src/controllers/CharacterController.ts
@@ -5,6 +5,12 @@ export class CharacterController{
     constructor(private service: ICharacterService){}
     
     async getAliveCharacters(req: Request, res: Response): Promise<void> {
-        res.json({results: []});
+        try{
+            const data = await this.service.getAliveCharacters(); 
+            res.json(data); //convertir a JSON y enviar la respuesta
+        }
+        catch(error){
+            res.status(500).json({ error: 'Error Fetching characters' });
+        }
     }
 }
