78b455fca37f8170278edb25f771144a3f593e5d
Karen Sosa <l22020987@veracruz.tecnm.mx>
2026-02-12 00:26:51 -0600

    implementación de extracción de personajes desde la API

--- rick-morty-api/src/repositories/CharacterRepository.ts
+++ rick-morty-api/src/repositories/CharacterRepository.ts
@@ -1,7 +1,42 @@
-import { Character } from '../models/Character';
+import { Character, ApiCharacter, CharacterResponse } from '../models/Character';
 
-export class CharacterRepository {
-    async fetchAllCharacters(): Promise<Character[]> {
-        return [];
+//inyección de dependencias para facilitar el testing
+export interface ICharacterRepository {
+    fetchAllCharacters(): Promise<Character[]>;
+}
+
+//obtener datos de la Api Rick and Morty
+export class CharacterRepository implements ICharacterRepository {
+    private readonly apiUrl: string = 'https://rickandmortyapi.com/api';
+
+    async fetchAllCharacters(): Promise<ApiCharacter[]> {
+        const allCharacters: ApiCharacter[] = [];
+        let nextUrl: string | null = `${this.apiUrl}/character`;
+
+        while(nextUrl){
+            const pageData = await this.fetchPage(nextUrl);
+            allCharacters.push(...pageData.results);
+            nextUrl = pageData.info.next;
+        }
+        return allCharacters;
+    }
+
+
+    //función para obtener los datos de cada página de la API
+    private async fetchPage(url: string): Promise<CharacterResponse> {
+        try {
+            const response = await fetch(url);
+            if(!response.ok){
+                throw new Error(`Error fetching characters: ${response.status}`); //error 500
+            }
+            
+            const data: CharacterResponse = await response.json(); // data del personaje
+            return data;
+        } catch (error) {
+            if(error instanceof Error){
+                throw error;
+            }
+            throw new Error('Unknown error occurred while fetching character data');
+        }
     }
 }
